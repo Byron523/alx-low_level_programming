957725553a32e9e08a2e2d8c8a4565d08c43812c
Byron523 <byrongumbo23@outlook.com>
2022-09-08 00:39:49 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -> entry point
- *
- * return 0 for success
- */
+* main -> entry point
+*
+* Return 0 for success
+*/
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+  return (0);
 }
