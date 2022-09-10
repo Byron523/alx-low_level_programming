a88349fc58dd2d82fb55dcb2a5e536f9e77712bb
Byron523 <byrongumbo23@outlook.com>
2022-09-09 18:02:35 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - prints aphabet in lower and uppercase
+*
+* Return: zero for success
+*/
+
+int main(void)
+{
+	char low[26] = "abcdefghijklmnopqrstuvwxyz";
+	char upp[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int a;
+
+	for (a = 0; a < 26; a++)
+	{
+		putchar(low[a]);
+		putchar(upp[a]);
+	}
+	putchar('\n');
+	return (0);
+}
