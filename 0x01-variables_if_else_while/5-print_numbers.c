98f364092f80740abde5c6ab8af4c15573461031
Byron523 <byrongumbo23@outlook.com>
2022-09-09 18:25:11 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints the numbers 0 - 9
+*
+* Return: zero for success
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
