231c80f2c0f1fe63731d4264c26c1d9de145b5a3
Byron523 <byrongumbo23@outlook.com>
2022-09-09 19:17:03 -0700

    Patience, persitance and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - prints possible combinations of single-digits numbers
+*
+* Return: zero for success
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58: i++)
+	{
+		putchar(i);
+
+		if ( i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
