037634e5879c8f805b0d2ad6270abfd39c47e10e
Byron523 <byrongumbo23@outlook.com>
2022-09-13 13:54:31 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,13 @@
 * Return: void
 */
 
-coid print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for j = 97; j < 123; j++)
+		for (j = 97; j < 123; j++)
 		{
 			_putchar(j);
 		}
