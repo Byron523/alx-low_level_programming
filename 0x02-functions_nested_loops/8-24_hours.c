a5d48e44265870f1de8f6d2bd1e9ab73176d6f56
Byron523 <byrongumbo23@outlook.com>
2022-09-13 15:31:30 -0700

    I.m federal agent jack bauer and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,11 +9,11 @@ void jack_bauer(void)
 {
 	int i, j, k, a;
 
-	for (i = 48; i < 58; i++)
+	for (i = 48; i < 51; i++)
 	{
 		for (j = 48; j < 58; j++)
 		{
-			for (k = 48; k < 58; k++)
+			for (k = 48; k < 55; k++)
 			{
 				for (a = 48; a < 58; a++)
 				{
