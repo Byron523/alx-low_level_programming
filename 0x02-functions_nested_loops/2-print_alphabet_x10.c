8134e8a4f9449e4e78484d6b2eb3916fc4aeda2e
Byron523 <byrongumbo23@outlook.com>
2022-09-13 13:52:29 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - return a list ofalpha
+*
+* Return: void
+*/
+
+coid print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for j = 97; j < 123; j++)
+		{
+			_putchar(j);
+		}
+	}
+	_putchar('\n');
+}
