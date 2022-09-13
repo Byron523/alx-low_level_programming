2905bc99e62e12101c15b73d5a37a0afb2a773de
Byron523 <byrongumbo23@outlook.com>
2022-09-13 14:01:55 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _islower - return lower chaarcters
+*
+* Return: O if upper
+*/
+
+int _islower(int c)
+{
+	if (97 <= c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
