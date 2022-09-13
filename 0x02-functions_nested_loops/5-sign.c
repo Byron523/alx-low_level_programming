3790484f21160e859d75295040187e0695d692ce
Byron523 <byrongumbo23@outlook.com>
2022-09-13 14:28:51 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_sign - print sign of a number
+* @n: input value
+* Return: 1 to -1
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar(43);
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar(48);
+	}
+	else
+	{
+		return (-1);
+		_putchar(45);
+	}
+}
