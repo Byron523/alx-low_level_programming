c3db5f18bbaebeba2c27d71246092c2b5ea941dc
Byron523 <byrongumbo23@outlook.com>
2022-09-15 10:02:26 -0700

    Printing square of a number

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_square - print square o fn
+* @size: size of input
+* Return: non
+*/
+
+void print_square(int szie)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+	}
+	_putchar('\n');
+}
