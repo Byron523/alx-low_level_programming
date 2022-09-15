49901e4a2a88a76c12b65d9ca2385a1054804e61
Byron523 <byrongumbo23@outlook.com>
2022-09-15 07:39:27 -0700

    Prints numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - prints 0 to 9 numbers
+* Return: nothing
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+
+	_puchar('\n');
+}
