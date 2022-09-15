45c7878c3f776304f7b6ce4c451e7e77ae335c2a
Byron523 <byrongumbo23@outlook.com>
2022-09-15 08:24:36 -0700

    printing a dash

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,9 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
+	for (i = 0; i < n; i++)
 	{
-		continue;
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar(95);
-		}
+		_putchar(95);
 	}
 	_putchar('\n');
 }
