4c79b092643b2883ff661f223ef3a320166057e7
Byron523 <byrongumbo23@outlook.com>
2022-09-26 13:22:36 -0700

    memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _memset - replaces chars with another
+* @s: input string
+* @b: character to replace with
+* @n: number of bytes
+* Return: a character
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+These projects continue more on pointers and arrays. Introduction of 2D arrays is also encountered etc.
