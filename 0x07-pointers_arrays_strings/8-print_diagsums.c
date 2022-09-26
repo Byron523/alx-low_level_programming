e2a01ed84e817957a67f19422ae457b055a91312
Byron523 <byrongumbo23@outlook.com>
2022-09-26 14:38:50 -0700

    diagonal print

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_diagsums -prints sunt of two
+* diagonal arrays
+* @a: input
+* @size: size of array
+* return: nothing
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i, j = 0, k = 0;
+
+	for (i = 0; i < (size * size); i++)
+	{
+		if (i % (size + 1) == 0)
+		{
+			j += *(a + i);
+		}
+
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			k =+ *(a + i);
+	}
+	printf("%d, %d\n", j, k);
+}
