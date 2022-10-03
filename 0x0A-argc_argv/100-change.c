8d30767f64cdee520ceca1e52ecabcefe46d182d
Byron523 <byrongumbo23@outlook.com>
2022-10-03 11:44:04 -0700

    my change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,46 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - counts the fewest cins possible
-* @argc: argument counts
-* @argv: argument vector
-* Return: return 0 for success
+* main - finds the fewest number of coins
+* @argc: argument count
+* @argv: arguments inputs
+* Return: 0 for success 1 otherwise
 */
 int main(int argc, char *argv[])
 {
+	int i, j = 0;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int i = atoi(argv[1]), j = 0;
+	i = atoi(argv[1]);
 
-	while (i >= 25)
+	while (i > 0)
 	{
-		i -= 25;
+		if (i >= 25)
+			i -= 25;
+		if (i >= 10)
+			i -= 10;
+		if (i >= 5)
+			i -= 5;
+		if (i >= 2)
+			i -= 2;
+		if (i >= 1)
+			i -= 1;
 		j++;
 	}
-	while (i >= 10)
-	{
-		i -= 10;
-		j++;
-	}
-	while (i >= 5)
-	{
-		i -= 5;
-		j++;
-	}
-	while (i >= 2)
-	{
-		i -= 2;
-		j++;
-	}
-	while (i >= 1)
-	{
-		i -= 1;
-		j++;
-	}
-
 	printf("%d\n", j);
 	return (0);
 }
