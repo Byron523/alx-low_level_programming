12faffb48a234d63fdf2d12e517a06e2c7f5a19b
Byron523 <byrongumbo23@outlook.com>
2022-10-03 13:31:22 -0700

    bundle

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -18,8 +18,8 @@ int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-char *_strpbrk(char *s, char *acccept);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *acccept);
 char *_strstr(char *haystack, char *needle);
 
 #endif /* main.h */
