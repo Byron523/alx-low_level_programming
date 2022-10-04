6442ce85e61719334ee808c32944b38c49ac8aa0
Byron523 <byrongumbo23@outlook.com>
2022-10-04 06:48:51 -0700

    grdi

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* alloc_grid - allocates a grid
+* @width: input withd
+* @height: input height
+* Return: an int
+*/
+int **alloc_grid(int width, int height)
+{
+	int **m;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	m = malloc(width * sizeof(int *));
+	if (m == NULL)
+	{
+		free(m);
+		return (NULL);
+	}
+
+	for (i = 0; i < width; i++)
+	{
+		m[i] = malloc(height * sizeof(int));
+		if (m[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+			     free(m[i]);
+			free(m);
+			return (NULL);
+		}
+	}
+
+	for ( i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			m[i][j] = 0;
+
+	return (m);
+}
