e1deb6c18586d773b58c759ed08ee93735b2b22d
Byron523 <byrongumbo23@outlook.com>
2022-10-12 07:27:51 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+* print_name - is a function that prints name
+* of a person
+* @name: name of a person
+* @f: function pointer
+* Return: non
+*/
+void print_name(char *name, void (*f)(char *));
+{
+	if (name && f)
+	{
+		(*f)(name);
+	}
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+This folder contains functions that consist of function pointers.'
+They start from simple use upto advnaced usaged of pointer functions and other aspects best use for them
