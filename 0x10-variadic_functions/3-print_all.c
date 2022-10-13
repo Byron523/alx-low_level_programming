4410124abe20a3ed3c36e4b4bee9a9de552d95a5
Byron523 <byrongumbo23@outlook.com>
2022-10-13 14:34:40 -0700

    40 lines

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* print_all - prints anything given
+* @format: input format
+* Return: nothing
+*/
+void print_all(const char * const format, ...)
+{
+	va_list ptr;
+	unsigned int i = 0, j, k = 0;
+	char *s;
+	const char type[] ="cifs";
+
+	va_start(ptr, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (type[j])
+		{
+			if (format[i] == type[j] && k)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+			case 'c':
+				prints("%c", var_arg(ptr, int)), k = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(ptr, int)), k = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(ptr, double)), k = 1;
+				break;
+			case 's':
+				s = va_arg(ptr, char *), k = 1;
+				if (s)
+					printf("%s", s);
+				else
+				{
+					printf("(nil)");
+					break;
+				}
+		} i++;
+	}
+
+	printf("\n");
+	va_end(ptr);
+}
