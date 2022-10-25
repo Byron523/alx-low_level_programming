4a5dacf0cbbfa778ab9e7bb8abc03532ec72a76b
Byron523 <byrongumbo23@outlook.com>
2022-10-25 12:07:19 -0700

    node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,11 +13,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	i = 0;
 	while (head != NULL)
 	{
-		i++;
-		if (i != index)
+		if (i < index)
 		{
 			head = head->next;
 		}
+		i++;
 	}
 	return (head);
 }
