8cd93771c0b4cc84894cec370c4df494902c4d06
Byron523 <byrongumbo23@outlook.com>
2022-11-03 15:00:47 -0700

    append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text to end of a file
+ * @filename: name of the file
+ * @text_content: a null termianted string
+ * Return: 1 on success -1 otherwise
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, numl, in;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (numl = 0; text_content[numl]; numl++)
+			;
+
+		in = write(fd, text_content, numl);
+		if (in == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
